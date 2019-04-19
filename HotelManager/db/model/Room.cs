bf5ad5d48e2015b1bbb71c127656468a128cb68d
Thanh Sơn Bành <takashato@gmail.com>
2019-04-19 12:13:21 +0700

    Add Room model

---
+++ HotelManager/db/model/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManager.db.model
+{
+    class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Price { get; set; }
+        public string Note { get; set; }
+        public int Status { get; set; } = 0;
+    }
+}
