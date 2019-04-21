8b43273bda7fa10ae0efec27fa4569d2e737f6cd
PTChinh <PTChinh@users.noreply.github.com>
2019-04-21 17:55:40 +0700

    Add RoomType model

---
+++ HotelManager/db/model/RoomType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HotelManager.db.model
+{
+    public class RoomType
+    {
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+
+        public static List<RoomType> GetRoomType()
+        {
+            using (var conn = DatabaseManager.Conn)
+            {
+                return conn.Query<RoomType>("SELECT * FROM `room_type`").ToList();
+            }
+        }
+    }
+}
