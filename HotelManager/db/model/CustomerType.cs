a83578500cfbe218fa4433d7a4c36e3eac4b7890
PTChinh <PTChinh@users.noreply.github.com>
2019-04-25 16:35:00 +0700

    Add CustomerType model

---
+++ HotelManager/db/model/CustomerType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HotelManager.db.model
+{
+    public class CustomerType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public float Surcharge { get; set; }
+        public string Note { get; set; }
+
+
+        public static List<CustomerType> GetCustomerType()
+        {
+            using (var conn = DatabaseManager.Conn)
+            {
+                return conn.Query<CustomerType>("SELECT * FROM customer_type").ToList();
+            }
+        }
+    }
+}
