3efd5df0f78b03d84f88d1457fdf702388ee5e5f
PTChinh <PTChinh@users.noreply.github.com>
2019-04-25 16:07:25 +0700

    Update hotelmanager.sql

--- HotelManager/sql/hotelmanager.sql
+++ HotelManager/sql/hotelmanager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2019 at 01:21 PM
+-- Generation Time: Apr 25, 2019 at 11:05 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -25,6 +25,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) CHARACTER SET utf8 NOT NULL,
+  `id_card_number` bigint(20) NOT NULL,
+  `address` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `room_rental_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `name`, `id_card_number`, `address`, `type`, `room_rental_date`) VALUES
+(1, 'Nguyễn Văn Tuấn', 2511252515, 'Hà Nội', 'Inland', '2019-04-25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_type`
+--
+
+CREATE TABLE `customer_type` (
+  `type` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `surcharge` float NOT NULL,
+  `note` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer_type`
+--
+
+INSERT INTO `customer_type` (`type`, `surcharge`, `note`) VALUES
+('Inland', 3, ''),
+('foreign', 10, '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `room`
 --
 
@@ -32,6 +74,7 @@ CREATE TABLE `room` (
   `id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
   `type` varchar(128) NOT NULL,
+  `status` enum('Available','NotAvailable') NOT NULL,
   `note` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -39,9 +82,10 @@ CREATE TABLE `room` (
 -- Dumping data for table `room`
 --
 
-INSERT INTO `room` (`id`, `name`, `type`, `note`) VALUES
-(1, 'B757', 'B', 'provip'),
-(2, 'A999', 'A', '');
+INSERT INTO `room` (`id`, `name`, `type`, `status`, `note`) VALUES
+(1, 'B757', 'B', 'Available', 'dịch vụ vip'),
+(2, 'A999', 'A', 'Available', 'dịch vụ vip'),
+(3, 'C199', 'C', 'Available', '');
 
 -- --------------------------------------------------------
 
@@ -92,6 +136,13 @@ INSERT INTO `staff` (`id`, `username`, `password`, `fullname`, `lastLoginDate`, 
 --
 
 --
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_card_number` (`id_card_number`);
+
+--
 -- Indexes for table `room`
 --
 ALTER TABLE `room`
@@ -112,10 +163,16 @@ ALTER TABLE `staff`
 --
 
 --
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `staff`
