deffd61676b0da5372c2f4cc466c2fef514df1b6
PTChinh <PTChinh@users.noreply.github.com>
2019-05-08 15:14:13 +0700

    Add row header of list customer in RentingWindow

--- HotelManager/gui/RoomListTab/RentingWindow.xaml.cs
+++ HotelManager/gui/RoomListTab/RentingWindow.xaml.cs
@@ -15,6 +15,8 @@ using HotelManager.db.model;
 using System.Data;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Collections.Specialized;
+using System.Windows.Controls.Primitives;
 
 namespace HotelManager.gui
 {
@@ -52,6 +54,93 @@ namespace HotelManager.gui
             // TODO: Update Danh sách khách hàng thuê phòng cho _roomToRent
             this.Close();
         }
+
+        private void DataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();          
+        }
+
+    }
+
+    public class DataGridBehavior //Copy trên stackoverflow để fix lỗi khi xóa hoặc thêm một dòng thì row header không update
+    {
+        #region DisplayRowNumber
+
+        public static DependencyProperty DisplayRowNumberProperty =
+            DependencyProperty.RegisterAttached("DisplayRowNumber",
+                                                typeof(bool),
+                                                typeof(DataGridBehavior),
+                                                new FrameworkPropertyMetadata(false, OnDisplayRowNumberChanged));
+        public static bool GetDisplayRowNumber(DependencyObject target)
+        {
+            return (bool)target.GetValue(DisplayRowNumberProperty);
+        }
+        public static void SetDisplayRowNumber(DependencyObject target, bool value)
+        {
+            target.SetValue(DisplayRowNumberProperty, value);
+        }
+
+        private static void OnDisplayRowNumberChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            DataGrid dataGrid = target as DataGrid;
+            if ((bool)e.NewValue == true)
+            {
+                EventHandler<DataGridRowEventArgs> loadedRowHandler = null;
+                loadedRowHandler = (object sender, DataGridRowEventArgs ea) =>
+                {
+                    if (GetDisplayRowNumber(dataGrid) == false)
+                    {
+                        dataGrid.LoadingRow -= loadedRowHandler;
+                        return;
+                    }
+                    ea.Row.Header = ea.Row.GetIndex() + 1;
+                };
+                dataGrid.LoadingRow += loadedRowHandler;
+
+                ItemsChangedEventHandler itemsChangedHandler = null;
+                itemsChangedHandler = (object sender, ItemsChangedEventArgs ea) =>
+                {
+                    if (GetDisplayRowNumber(dataGrid) == false)
+                    {
+                        dataGrid.ItemContainerGenerator.ItemsChanged -= itemsChangedHandler;
+                        return;
+                    }
+                    GetVisualChildCollection<DataGridRow>(dataGrid).
+                        ForEach(d => d.Header = d.GetIndex() + 1);
+                };
+                dataGrid.ItemContainerGenerator.ItemsChanged += itemsChangedHandler;
+            }
+        }
+
+        #endregion // DisplayRowNumber
+
+        #region Get Visuals
+
+        private static List<T> GetVisualChildCollection<T>(object parent) where T : Visual
+        {
+            List<T> visualCollection = new List<T>();
+            GetVisualChildCollection(parent as DependencyObject, visualCollection);
+            return visualCollection;
+        }
+
+        private static void GetVisualChildCollection<T>(DependencyObject parent, List<T> visualCollection) where T : Visual
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                if (child is T)
+                {
+                    visualCollection.Add(child as T);
+                }
+                if (child != null)
+                {
+                    GetVisualChildCollection(child, visualCollection);
+                }
+            }
+        }
+
+        #endregion // Get Visuals
     }
 
     public enum LoaiKhachHang
