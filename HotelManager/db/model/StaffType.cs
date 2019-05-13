1fc7a13565fb3b8d33eb93addbfdc31d3ddd71e6
PTChinh <PTChinh@users.noreply.github.com>
2019-05-13 14:22:20 +0700

    Add StaffType model

---
+++ HotelManager/db/model/StaffType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HotelManager.db.model
+{
+    public class StaffType
+    {
+        public int Level { get; set; }
+        public string Type { get; set; }
+
+        public static List<string> GetStaffTypes()
+        {
+            using (var conn = DatabaseManager.Conn)
+            {
+                return conn.Query<string>("SELECT `type` FROM `staff_type`").ToList();
+            }
+        }
+
+    }
+}
