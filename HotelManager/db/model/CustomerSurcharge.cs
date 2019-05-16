bc6c371b20ab62a5dbe0634e5e7886f01010e8b5
PTChinh <PTChinh@users.noreply.github.com>
2019-05-16 16:06:51 +0700

    Add CustomerSurcharge model

---
+++ HotelManager/db/model/CustomerSurcharge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HotelManager.db.model
+{
+    public class CustomerSurcharge
+    {
+        public int Quantum { get; set; }
+        public float Surcharge { get; set; }
+        public string Note { get; set; }
+
+        public static List<CustomerSurcharge> GetAll()
+        {
+            using (var conn = DatabaseManager.Conn)
+            {
+                return conn.Query<CustomerSurcharge>("SELECT * FROM customer_surcharge").ToList();
+            }
+        }
+    }
+}
