4e6ab0a542f7c46d0ff60ab047d9d88664d8a885
PTChinh <PTChinh@users.noreply.github.com>
2019-05-23 17:29:11 +0700

    Add RoomRentalDetail model

---
+++ HotelManager/db/model/RoomRentalDetail.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HotelManager.db.model
+{
+    public class RoomRentalDetail
+    {
+        public string RoomName { get; set; }
+        public string CustomerName { get; set; }
+        public decimal CustomerID { get; set; }
+        public string Address { get; set; }
+        public string CustomerType { get; set; }
+
+        public static bool InsertRoomRentalDetail(string roomName, string customerName, decimal customerID, string address, string customerType)
+        {
+            using (var conn = DatabaseManager.Conn)
+            {
+                RoomRentalDetail roomRentalDetail = new RoomRentalDetail { RoomName = roomName, CustomerName = customerName, CustomerID = customerID, Address = address, CustomerType = customerType };
+                try
+                {
+                    return conn.Execute("INSERT INTO room_rental_detail(room_name, customer_name, customer_id, address, customer_type) VALUE(@RoomName, @CustomerName, @CustomerID, @Address, @CustomerType)",roomRentalDetail) > 0;
+                }
+                catch(Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static bool DeleteRoomRentalDetail(string roomName)
+        {
+            using (var conn = DatabaseManager.Conn)
+            {
+                return conn.Execute("DELETE FROM room_rental_detail WHERE room_name = @RoomName", new { RoomName = roomName}) > 0;
+            }
+        }
+    }
+}
