9e144b8e49a585ac7e5fe4203a35486c4c603c2b
PTChinh <PTChinh@users.noreply.github.com>
2019-05-31 16:55:05 +0700

    Add PaymentDetail model

---
+++ HotelManager/db/model/PaymentDetail.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace HotelManager.db.model
+{
+    public class PaymentDetail
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public DateTime CheckinDate { get; set; }
+        public int DaysRented { get; set; }
+        public int CustomerQuantum { get; set; }
+        public int ForeignQuantum { get; set; }
+        public double Amount { get; set; }
+
+        public static bool InsertPaymentDetail(string roomName, DateTime checkinDate, int customerQuantum, int foreignQuantum)
+        {
+            using (var conn = DatabaseManager.Conn)
+            {
+                var paymentDetail = new PaymentDetail { RoomName = roomName, CheckinDate = checkinDate, CustomerQuantum = customerQuantum, ForeignQuantum = foreignQuantum};
+                return conn.Execute("INSERT INTO payment_detail(room_name, checkin_date, customer_quantum, foreign_quantum) VALUES(@RoomName, @CheckinDate, @CustomerQuantum, @ForeignQuantum)", paymentDetail) > 0;
+            }
+        }
+
+        public static bool UpdatePaymentDetail(string roomName, int daysRented, double amount)
+        {
+            using (var conn = DatabaseManager.Conn)
+            {
+               try
+                {
+                    return conn.Execute("UPDATE payment_detail SET days_rented = @DaysRented, amount = @Amount WHERE room_name = @RoomName", new { RoomName = roomName, DaysRented = daysRented, Amount = amount }) > 0;
+                }
+                catch(Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static List<PaymentDetail> GetPaymentDetailByRoomName()
+        {
+            using (var conn = DatabaseManager.Conn)
+            {
+                return conn.Query<PaymentDetail>("SELECT room_name, checkin_date, days_rented, customer_quantum, foreign_quantum, amount FROM payment_detail").ToList();
+            }
+        }
+    }
+}
